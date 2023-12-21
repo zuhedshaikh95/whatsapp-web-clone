662678e24694c644eefeaf2ce61658169844c47f
zuhedshaikh95 <zuhed95@gmail.com>
2023-12-22 00:39:44 +0530

    add: configs - prisma, firebase, postgres

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,17 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import crypto from "crypto";
+import cors from "cors";
 
 // For .env file
 dotenv.config();
 
 const app: Express = express();
 const PORT = 8080;
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (request: Request, response: Response) => {
   response.send("Hello, Topper!");
