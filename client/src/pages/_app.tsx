481feb4b50983283f3136dfc9edacbd5608d5f00
zuhedshaikh95 <zuhed95@gmail.com>
2024-01-07 16:58:19 +0530

    add: redux Provider

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,14 +1,19 @@
-import type { AppProps } from "next/app";
 import "@/styles/globals.css";
+import store from "@/redux/store";
+import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>WhatsApp Web</title>
         <link rel="icon" href="favicon.png" />
       </Head>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </>
   );
 }
