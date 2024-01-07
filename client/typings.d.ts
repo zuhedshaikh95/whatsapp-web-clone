c7eae3f08c98e6fa949622806f0a34da4500bd10
zuhedshaikh95 <zuhed95@gmail.com>
2024-01-07 17:00:12 +0530

    add: UserStateI interface

--- client/typings.d.ts
+++ client/typings.d.ts
@@ -2,3 +2,10 @@ export interface User {
   id: number;
   email: string;
 }
+
+interface UserStateI {
+  userMessage: string;
+  user: User | null;
+  userError: boolean;
+  userLoading: boolean;
+}
